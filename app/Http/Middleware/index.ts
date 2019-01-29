5315339b036fd09044e52846aa88d74579cff6e5
Damen <damen@damenhannah.com>
2019-01-29 15:53:05 -0600

    Fixed middleware namespace comment.

--- app/Http/Middleware/index.ts
+++ app/Http/Middleware/index.ts
@@ -3,7 +3,7 @@
 | The Middleware Namespace
 |--------------------------------------------------------------------------
 |
-| All controllers that intend to be used by the application should be declared here.
+| All middleware that intend to be used by the application should be declared here.
 | This might change to an actual namespace. I don't know. Trying to get to three c.. oops. I went over.
 |
 */
